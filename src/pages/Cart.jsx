3efd60dab3a5bbc8fb4490eeb2e64f82aed13dec
MarinaArrieta <arrietamarina12@gmail.com>
2025-02-27 20:10:54 -0300

    Etiqueta card, card responsive

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,56 +1,49 @@
 import {
     Box,
     Button,
-    FormControl,
-    FormErrorMessage,
-    FormLabel,
-    Input,
-    InputGroup,
-    InputRightElement,
+    Card,
+    CardBody,
+    CardFooter,
+    Heading,
+    Image,
+    Select,
+    Stack,
+    Text,
+    VStack,
 } from "@chakra-ui/react";
 
 export const Cart = () => {
 
     return (
-        <Box maxW="400px" mx="auto" mt="10">
-            <form>
-                <FormControl>
-                    <FormLabel htmlFor="email" color='#ed5940'>Tus compras</FormLabel>
-                    <Input
-                        type="email"
-                        id="email"
-                        placeholder="Ingresa tu nombre/usuario"
-                        _placeholder={{ color: '#8d803e' }}
-                        borderColor='#f7b3cd'
-                        focusBorderColor='#ffbb00'
-                        // {...register("email", email)}
-                    />
-                    {/* <FormErrorMessage>{errors.email?.message}</FormErrorMessage> */}
-                </FormControl>
-                <FormControl>
-                    <FormLabel htmlFor="password" color='#ed5940'>Contraseña</FormLabel>
-                    <InputGroup size="md">
-                        <Input
-                            id="password"
-                            pr="4.5rem"
-                            // type={show ? "text" : "password"}
-                            placeholder="Ingrese su contraseña"
-                            _placeholder={{ color: '#8d803e' }}
-                            borderColor='#f7b3cd'
-                            focusBorderColor='#ffbb00'
-                            // {...register("password", password)}
-                        />
-                        <InputRightElement width="4.5rem">
-                            <Button h="1.75rem" size="sm" colorScheme='orange'>
-                            </Button>
-                        </InputRightElement>
-                    </InputGroup>
-                    {/* <FormErrorMessage>{errors.password?.message}</FormErrorMessage> */}
-                </FormControl>
-                <Button mt={4} type="submit" width="100%" colorScheme='pink'>
-                    Registrarme
-                </Button>
-            </form>
-        </Box>
+        <VStack p='35px'>
+            <Card
+                direction={{ base: 'column', sm: 'row' }}
+                overflow='hidden'
+                variant='outline'
+            >
+                <Image
+                    objectFit='cover'
+                    maxW={{ base: '100%', sm: '200px' }}
+                    src='https://images.unsplash.com/photo-1667489022797-ab608913feeb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw5fHx8ZW58MHx8fHw%3D&auto=format&fit=crop&w=800&q=60'
+                    alt='Caffe Latte'
+                />
+                <Stack>
+                    <CardBody>
+                        <Heading size='md'>The perfect latte</Heading>
+                        <Text py='2'>
+                            Elejí la cantidad de MUU que quieras comprar
+                        </Text>
+                        <Select >
+                            <option value='option1'>1</option>
+                        </Select>
+                    </CardBody>
+                    <CardFooter>
+                        <Button variant='solid' colorScheme='blue'>
+                            Realizar compra
+                        </Button>
+                    </CardFooter>
+                </Stack>
+            </Card>
+        </VStack>
     );
 };
