b21ef083a3edb2cb99c852a06a63d4d7c7c7531f
MarinaArrieta <arrietamarina12@gmail.com>
2025-02-27 03:36:16 -0300

    Cambios de estilos para login, nuevos colores

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -37,38 +37,42 @@ export const Login = () => {
     }
 
     return (
-        <Box maxW="400px" mx="auto" mt="10">
+        <Box maxW="300px" mx="auto" mt="10">
             <form onSubmit={handleSubmit(onSubmit)}>
                 <FormControl isInvalid={errors.email}>
-                    <FormLabel htmlFor="email">Usuario</FormLabel>
+                    <FormLabel htmlFor="email" color='#ed5940'>Usuario</FormLabel>
                     <Input
                         type="email"
                         id="email"
                         placeholder="Ingresa tu nombre/usuario"
+                        _placeholder={{ color: '#8d803e' }}
+                        borderColor='#f7b3cd'
                         {...register("email", email)}
                     />
                     <FormErrorMessage>{errors.email?.message}</FormErrorMessage>
                 </FormControl>
                 <FormControl isInvalid={errors.password}>
-                    <FormLabel htmlFor="password">Contraseña</FormLabel>
+                    <FormLabel htmlFor="password" color='#ed5940'>Contraseña</FormLabel>
                     <InputGroup size="md">
                         <Input
                             id="password"
                             pr="4.5rem"
                             type={show ? "text" : "password"}
                             placeholder="Ingrese su contraseña"
+                            _placeholder={{ color: '#8d803e' }}
+                            borderColor='#f7b3cd'
                             {...register("password", password)}
                         />
                         <InputRightElement width="4.5rem">
-                            <Button h="1.75rem" size="sm" onClick={handleClick}>
+                            <Button h="1.75rem" size="sm" colorScheme='orange' onClick={handleClick}>
                                 {show ? "Hide" : "Show"}
                             </Button>
                         </InputRightElement>
                     </InputGroup>
                     <FormErrorMessage>{errors.password?.message}</FormErrorMessage>
                 </FormControl>
-                <Button mt={4} colorScheme="teal" type="submit" width="100%">Iniciar sesión</Button>
-                <Button onClick={handleGoogleSignIn} mt={4} colorScheme="teal" type="button" width="100%">
+                <Button mt={4} colorScheme="teal" type="submit" width="100%" colorScheme='pink'>Iniciar sesión</Button>
+                <Button onClick={handleGoogleSignIn} mt={4} colorScheme='pink' type="button" width="100%">
                     Iniciar sesión con google
                 </Button>
             </form>
