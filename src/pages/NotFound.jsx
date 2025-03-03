8531dbc5b3cdde2e680e67aa284cb9f449e1125e
MarinaArrieta <arrietamarina12@gmail.com>
2025-03-03 19:24:16 -0300

    Agregue la etiqueta de card para el texto y imagen

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,18 +1,21 @@
-import { Image, Text, VStack } from "@chakra-ui/react"
+import { Card, CardBody, Image, Stack, Text, VStack } from "@chakra-ui/react"
 import error from '../assets/error-404.png'
 
 const NotFound = () => {
   return (
     <VStack>
-      <Text color='#5f5525' fontSize='2xl'>La página que estas buscando no se encuentra</Text>
-      <Image
-        boxSize='170px'
-        objectFit='cover'
-        w={{ base: '300px', md: '480px', lg: '560px' }}
-        h='unset'
-        src={error}
-        alt='Logo vaca'
-      />
+      <Card maxW='sm' bg='#f2e8d7' boxShadow='none'>
+        <Stack mt='6' spacing='3'>
+          <Text color='#5f5525' fontSize='2xl' m={{base:'1.25rem', md:'unset'}}>La página que estas buscando no se encuentra</Text>
+        </Stack>
+        <CardBody>
+          <Image
+            src={error}
+            alt='Desierto error 404'
+            borderRadius='lg'
+          />
+        </CardBody>
+      </Card>
     </VStack>
   )
 }
