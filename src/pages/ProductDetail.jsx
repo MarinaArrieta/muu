7934b1eeee5b9598b752d0e14695c1a292c529fa
MarinaArrieta <arrietamarina12@gmail.com>
2025-03-11 15:03:08 -0300

    Agregue loading con su spinner en el prodct detail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -9,6 +9,7 @@ import {
     Heading,
     HStack,
     Image,
+    Spinner,
     Stack,
     Text,
     useToast,
@@ -25,6 +26,7 @@ const ProductDetail = () => {
 
     const [product, setProduct] = useState({})
     const [products, setProducts] = useState([])
+    const [loading, setLoading] = useState(true)
     const { id } = useParams()
     const { user } = useAuth()
     const toast = useToast()
@@ -59,6 +61,8 @@ const ProductDetail = () => {
                 setProducts(data)
             } catch (error) {
                 setError(true)
+            } finally {
+                setLoading(false)
             }
         }
         if (user) {
@@ -93,90 +97,96 @@ const ProductDetail = () => {
             templateColumns={{ base: "1fr", sm: "1fr", lg: "repeat(1, 1fr)" }}
             gap={6}
         >
-            <VStack>
-                <Card maxW='sm' bg='#f2e8d700' shadow='unset'>
-                    <CardBody>
-                        <Image
-                            src={product.image_url}
-                            alt={product.name}
-                            filter='drop-shadow(2px 5px 4px #211714)'
-                            borderRadius='lg'
-                        />
-                        <Stack mt='6' spacing='3'>
-                            <Heading size='md' color='#ff77ad'>{product.name}</Heading>
-                            <Text color='#5f5525'>
-                                {product.description}
-                            </Text>
-                            <Text color='#ed5940' fontSize='2xl'>
-                                $ {product.price} c/u
-                            </Text>
-                        </Stack>
-                    </CardBody>
-                    <Divider />
-                    <CardFooter justify='end'>
-                        {!user ?
-                            <VStack alignItems='end'>
-                                <Text color='#5f5525'>Registrate o inicia sesión para poder comprar</Text>
-                                <ButtonGroup spacing='2'>
-                                    <Button variant='solid' colorScheme='pink'>
-                                        <Link to={`/register`}>Registrarse</Link>
+            {loading ?
+                <HStack w='100%' marginTop='35px' alignItems='center' justifyContent='center'>
+                    <Spinner size='xl' color='#ed5940' filter='drop-shadow(2px 5px 4px #ffb5a8)' thickness='10px' />
+                </HStack>
+                :
+                <VStack>
+                    <VStack>
+                        <Card maxW='sm' bg='#f2e8d700' shadow='unset'>
+                            <CardBody>
+                                <Image
+                                    src={product.image_url}
+                                    alt={product.name}
+                                    filter='drop-shadow(2px 5px 4px #211714)'
+                                    borderRadius='lg'
+                                />
+                                <Stack mt='6' spacing='3'>
+                                    <Heading size='md' color='#ff77ad'>{product.name}</Heading>
+                                    <Text color='#5f5525'>
+                                        {product.description}
+                                    </Text>
+                                    <Text color='#ed5940' fontSize='2xl'>
+                                        $ {product.price} c/u
+                                    </Text>
+                                </Stack>
+                            </CardBody>
+                            <Divider />
+                            <CardFooter justify='end'>
+                                {!user ?
+                                    <VStack alignItems='end'>
+                                        <Text color='#5f5525'>Registrate o inicia sesión para poder comprar</Text>
+                                        <ButtonGroup spacing='2'>
+                                            <Button variant='solid' colorScheme='pink'>
+                                                <Link to={`/register`}>Registrarse</Link>
+                                            </Button>
+                                            <Button variant='solid' colorScheme='pink'>
+                                                <Link to={`/login`}>Iniciar sesión</Link>
+                                            </Button>
+                                        </ButtonGroup>
+                                    </VStack>
+                                    :
+                                    <Button variant='solid' colorScheme='pink' onClick={addToCartClick} data-id={product.id}>
+                                        Agregar al carrito
                                     </Button>
-                                    <Button variant='solid' colorScheme='pink'>
-                                        <Link to={`/login`}>Iniciar sesión</Link>
-                                    </Button>
-                                </ButtonGroup>
-                            </VStack>
-                            :
-                            <Button variant='solid' colorScheme='pink' onClick={addToCartClick} data-id={product.id}>
-                                Agregar al carrito
-                            </Button>
-                        }
-                    </CardFooter>
-                </Card>
-            </VStack>
-
-            <VStack
-                spacing={4}
-                bg='radial-gradient(17% 23% at 92% 16%, #fcdba8 0%, rgb(255 200 218) 80%, rgb(158 158 158 / 0%) 209%), radial-gradient(44% 67% at 108% 51%, #ffddaa 40%, rgb(255 218 231 / 63%) 71%, rgba(76, 175, 80, 0) 121%), radial-gradient(41% 54% at 3% -6%, #ffeac6 14%, rgb(234 204 214) 28%, rgb(76 175 80 / 0%) 121%), radial-gradient(18% 55% at 49% 80%, rgb(235 241 180) 7%, rgb(255 234 198) 100%)'
-                borderRadius='20px'
-                p='15px'
-            >
-                <Text fontSize='2xl' color='#5f5525' as='i' p='35px 0px 0px' borderRadius='11px' >También te puede interesar...</Text>
-                <VStack
-                    gap={{ sm: '70px', lg: '90px' }}
-                    flexDirection={{ base: 'column', sm: 'row' }}
-                >
-                    {
-                        filteredProducts.map((product) => (
-                            <Card key={product.id} maxW='sm' bg='#f2e8d700' shadow='unset' w='263px' borderColor='#bd996b'>
-                                <CardBody>
-                                    <Image
-                                        src={product.image_url}
-                                        alt={product.name}
-                                        filter='drop-shadow(2px 5px 4px #211714)'
-                                        borderRadius='lg'
-                                    />
-                                    <Stack mt='6' spacing='3'>
-                                        <Heading size='md' color='#ff77ad'>{product.name}</Heading>
-                                        <Text color='#ed5940' fontSize='2xl'>
-                                            $ {product.price} c/u
-                                        </Text>
-                                    </Stack>
-                                </CardBody>
-                                <Divider />
-                                <CardFooter justify='end'>
-                                    <ButtonGroup spacing='2'>
-                                        <Button variant='solid' colorScheme='pink'>
-                                            <Link to={`/product-detail/${product.id}`}>Ver más</Link>
-                                        </Button>
-                                    </ButtonGroup>
-                                </CardFooter>
-                            </Card>
-                        ))
-                    }
+                                }
+                            </CardFooter>
+                        </Card>
+                    </VStack>
+                    <VStack
+                        spacing={4}
+                        bg='radial-gradient(17% 23% at 92% 16%, #fcdba8 0%, rgb(255 200 218) 80%, rgb(158 158 158 / 0%) 209%), radial-gradient(44% 67% at 108% 51%, #ffddaa 40%, rgb(255 218 231 / 63%) 71%, rgba(76, 175, 80, 0) 121%), radial-gradient(41% 54% at 3% -6%, #ffeac6 14%, rgb(234 204 214) 28%, rgb(76 175 80 / 0%) 121%), radial-gradient(18% 55% at 49% 80%, rgb(235 241 180) 7%, rgb(255 234 198) 100%)'
+                        borderRadius='20px'
+                        p='15px'
+                    >
+                        <Text fontSize='2xl' color='#5f5525' as='i' p='35px 0px 0px' borderRadius='11px' >También te puede interesar...</Text>
+                        <VStack
+                            gap={{ sm: '70px', lg: '90px' }}
+                            flexDirection={{ base: 'column', sm: 'row' }}
+                        >
+                            {
+                                filteredProducts.map((product) => (
+                                    <Card key={product.id} maxW='sm' bg='#f2e8d700' shadow='unset' w='263px' borderColor='#bd996b'>
+                                        <CardBody>
+                                            <Image
+                                                src={product.image_url}
+                                                alt={product.name}
+                                                filter='drop-shadow(2px 5px 4px #211714)'
+                                                borderRadius='lg'
+                                            />
+                                            <Stack mt='6' spacing='3'>
+                                                <Heading size='md' color='#ff77ad'>{product.name}</Heading>
+                                                <Text color='#ed5940' fontSize='2xl'>
+                                                    $ {product.price} c/u
+                                                </Text>
+                                            </Stack>
+                                        </CardBody>
+                                        <Divider />
+                                        <CardFooter justify='end'>
+                                            <ButtonGroup spacing='2'>
+                                                <Button variant='solid' colorScheme='pink'>
+                                                    <Link to={`/product-detail/${product.id}`}>Ver más</Link>
+                                                </Button>
+                                            </ButtonGroup>
+                                        </CardFooter>
+                                    </Card>
+                                ))
+                            }
+                        </VStack>
+                    </VStack>
                 </VStack>
-            </VStack>
-
+            }
         </Grid>
     )
 }
