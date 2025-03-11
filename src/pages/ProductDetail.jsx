1dc30a72d67fbca1728e5bae7befa3589806741e
MarinaArrieta <arrietamarina12@gmail.com>
2025-03-11 14:54:14 -0300

    Cambios de estilos en la card de los filtros en product detail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -7,6 +7,7 @@ import {
     Divider,
     Grid,
     Heading,
+    HStack,
     Image,
     Stack,
     Text,
@@ -17,7 +18,6 @@ import { Link, useParams } from 'react-router-dom'
 import { useState, useEffect } from "react"
 import { getProducts } from "../services/products"
 import { useAuth } from '../context/AuthContext'
-// import { auth } from "../firebase/config"
 import { addToCart } from './Cart'
 import conection from "../assets/conection.png"
 
@@ -84,7 +84,6 @@ const ProductDetail = () => {
     useEffect(() => {
         if (product?.category) {
             const filtered = products.filter(p => p.category === product.category && p.id !== product.id)
-            // .filter((p, index, self) => self.filter(item => item.id !== product.id))
             setFilteredProducts(filtered)
         }
     }, [product, products])
@@ -135,26 +134,32 @@ const ProductDetail = () => {
                     </CardFooter>
                 </Card>
             </VStack>
-            <VStack spacing={4}>
-                <Text fontSize='2xl' color='#5f5525' as='i' bgGradient='linear(to-r, #fbeaec, #f5ebc4, #fbeaec)' p='5px' borderRadius='11px' >También te puede interesar...</Text>
+
+            <VStack
+                spacing={4}
+                bg='radial-gradient(17% 23% at 92% 16%, #fcdba8 0%, rgb(255 200 218) 80%, rgb(158 158 158 / 0%) 209%), radial-gradient(44% 67% at 108% 51%, #ffddaa 40%, rgb(255 218 231 / 63%) 71%, rgba(76, 175, 80, 0) 121%), radial-gradient(41% 54% at 3% -6%, #ffeac6 14%, rgb(234 204 214) 28%, rgb(76 175 80 / 0%) 121%), radial-gradient(18% 55% at 49% 80%, rgb(235 241 180) 7%, rgb(255 234 198) 100%)'
+                borderRadius='20px'
+                p='15px'
+            >
+                <Text fontSize='2xl' color='#5f5525' as='i' p='35px 0px 0px' borderRadius='11px' >También te puede interesar...</Text>
                 <VStack
-                    gap={{sm:'70px', lg:'152px'}}
-                    flexDirection={{base:'column', sm:'row'}}
+                    gap={{ sm: '70px', lg: '90px' }}
+                    flexDirection={{ base: 'column', sm: 'row' }}
                 >
                     {
-                        filteredProducts.map((item) => (
-                            <Card key={item.id} maxW='sm' bg='#f2e8d700' shadow='unset' w='263px' borderColor='#bd996b'>
+                        filteredProducts.map((product) => (
+                            <Card key={product.id} maxW='sm' bg='#f2e8d700' shadow='unset' w='263px' borderColor='#bd996b'>
                                 <CardBody>
                                     <Image
-                                        src={item.image_url}
-                                        alt={item.name}
+                                        src={product.image_url}
+                                        alt={product.name}
                                         filter='drop-shadow(2px 5px 4px #211714)'
                                         borderRadius='lg'
                                     />
                                     <Stack mt='6' spacing='3'>
-                                        <Heading size='md' color='#ff77ad'>{item.name}</Heading>
+                                        <Heading size='md' color='#ff77ad'>{product.name}</Heading>
                                         <Text color='#ed5940' fontSize='2xl'>
-                                            $ {item.price} c/u
+                                            $ {product.price} c/u
                                         </Text>
                                     </Stack>
                                 </CardBody>
