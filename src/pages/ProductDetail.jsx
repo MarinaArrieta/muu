10e79e8e6abcd86e15c75f954c109660b919dd96
MarinaArrieta <arrietamarina12@gmail.com>
2025-03-12 15:07:12 -0300

    Cambie el boton de de los filtros en ver mas en product detail

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -202,9 +202,7 @@ const ProductDetail = () => {
                                         <Divider />
                                         <CardFooter justify='end'>
                                             <ButtonGroup spacing='2'>
-                                                <Button variant='solid' colorScheme='pink'>
-                                                    <Link to={`/product-detail/${product.id}`}>Ver más</Link>
-                                                </Button>
+                                                <Button variant='solid' colorScheme='pink' as={Link} to={`/product-detail/${product.id}`}>Ver más</Button>
                                             </ButtonGroup>
                                         </CardFooter>
                                     </Card>
